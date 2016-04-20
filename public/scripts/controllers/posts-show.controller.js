dbbed63e628d7bcc8e1eae74f492aead33e1a482
morgansharif <morgan.sharif@gmail.com>
2016-04-20 12:17:08 -0700

    added postsShowController

--- public/scripts/controllers/posts-show.controller.js
+++ public/scripts/controllers/posts-show.controller.js
@@ -2,4 +2,17 @@ PostsShowController.$inject = ["$location", "$http", "$routeParams"]; // minific
 function PostsShowController ($location, $http, $routeParams) {
   var vm = this;
   vm.post = {};
+  $http({
+    method: "GET",
+    url: "/api/posts/" + $routeParams.id
+  })
+  .then(onGetSuccess, onGetError);
+
+  function onGetSuccess(response){
+    vm.post = response.data;
+  }
+  function onGetError(response){
+  console.error("Failed to get post", response);
+  $location.path("/");
+  }
 }
